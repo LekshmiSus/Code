a609755f76d437603c060590cce50713fb92adef
LekshmiSus <131278761+LekshmiSus@users.noreply.github.com>
2023-04-19 21:11:22 +0530

    Update LargestNum.py

--- LargestNum.py
+++ LargestNum.py
@@ -3,12 +3,12 @@ import streamlit as st
 st.title("Find the Largest Number")
 
 # Input three numbers
-num_1 = st.number_input("Enter the first number")
-num_2 = st.number_input("Enter the second number")
-num_3 = st.number_input("Enter the third number")
+num1 = st.number_input("Enter the 1st number")
+num2 = st.number_input("Enter the 2nd number")
+num3 = st.number_input("Enter the 3rd number")
 
 # Find the largest number
-largest = max(num_1, num_2, num_3)
+largest = max(num1, num2, num3)
 
 # Display the result
 st.write("The largest number is:", largest)
