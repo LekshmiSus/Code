983d24db947307f75601130d132d0940f6462d4f
LekshmiSus <131278761+LekshmiSus@users.noreply.github.com>
2023-04-19 20:28:56 +0530

    Update LargestNum.py

--- LargestNum.py
+++ LargestNum.py
@@ -1,1 +1,17 @@
-print("largest number")
+import streamlit as st
+
+st.title("Larget=st Number")
+
+# Create the first text box
+text_1 = st.text_input("Enter first number")
+
+# Create the second text box
+text_2 = st.text_input("Enter second number")
+
+# Create the third text box
+text_3 = st.text_input("Enter third number")
+
+# Display the contents of the text boxes
+st.write("Text 1:", text_1)
+st.write("Text 2:", text_2)
+st.write("Text 3:", text_3)
